728dd9b8ec39ca1cf93a7f2ecd7cbb7c5f163124
Chao Deng <sheldon.d1018@gmail.com>
2015-11-20 00:02:41 +0800

    add qiniu and ignore test case to google

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -16,6 +16,8 @@ import (
 	_ "github.com/docker/distribution/registry/storage/driver/oss"
 	_ "github.com/docker/distribution/registry/storage/driver/s3"
 	_ "github.com/docker/distribution/registry/storage/driver/swift"
+
+	_ "github.com/docker/distribution/registry/storage/driver/qiniu"
 )
 
 func main() {
